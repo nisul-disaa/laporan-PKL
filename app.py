4e6bd11d416f9fc755967543fc16e325176e77f5
Annisa ahlaqul <annisaahlaqulk@gmail.com>
2025-10-14 14:10:30 +0700

    Upload proyek PKL

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pendahuluan')
+def pendahuluan():
+    return render_template('pendahuluan.html')
+
+@app.route('/profil')
+def profil():
+    return render_template('profil.html')
+
+@app.route('/biodata')
+def biodata():
+    return render_template('biodata.html')
+
+@app.route('/kompetensi')
+def kompetensi():
+    return render_template('kompetensi.html')
+
+@app.route('/kesan')
+def kesan():
+    return render_template('kesan.html')
+
+@app.route('/dokumentasi')
+def dokumentasi():
+    return render_template('dokumentasi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
